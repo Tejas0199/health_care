5a7b3353fd749102522bca45aa84505445526f91
ManojTechiee <manojmahieie@gmail.com>
2023-04-04 14:44:56 +0530

    completed router for login and signup container

--- src/App.js
+++ src/App.js
@@ -1,54 +1,30 @@
 import {useState} from 'react';
 import Input from "./component/Input";
 import Navbar from "./component/Navbar";
+import { Link, Route, Routes } from 'react-router-dom';
+import GreetingImageContainer from './component/GreetingImageContainer';
 
 
 function App() {
-  const [userInfo,setUserInfo]  = useState({
-    firstName : ""
-  })
-  const onChangeHandler = ({target : {name,value}}) => {
-    setUserInfo({...userInfo,[name] : value})
-  }
+  
   return (
     <div className="app">
       <Navbar/>
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        console.log(userInfo)
-      }}>
-      <div className="email-container">
-          <Input 
-          value={userInfo.firstName}
-          placeHolder={"Enter Name"}
-          onChange={onChangeHandler}
-          label="Enter First Name"
-          name="firstName"
-          error=""/>
-          <Input 
-          value={userInfo.lastName}
-          placeHolder={"Enter Last name"}
-          onChange={onChangeHandler}
-          label="Enter Last Name"
-          name="LastName"
-          error=""/>
-          <Input 
-          value={userInfo.fatherName}
-          placeHolder={"Enter Name"}
-          onChange={onChangeHandler}
-          label="Enter Father name"
-          name="fatherName"
-          error=""/>
-          <Input 
-          value={userInfo.email}
-          placeHolder={"Enter email"}
-          onChange={onChangeHandler}
-          label="Enter Gmail"
-          name="email"
-          error=""/>
-        <button>SUBMIT</button>
-      </div>
-      </form>
+      <Routes>
+        <Route index element={<h1>Hello Home</h1>}/>
+        <Route path='/login' element={<GreetingImageContainer>
+            <div>
+              <h1>Log-in container</h1>
+            <Link to="/signup">if not having account signup here</Link>
+            </div>
+        </GreetingImageContainer>}/>
+        <Route path='/signup' element={<GreetingImageContainer>
+            <div>
+              <h3>signup container</h3>
+              <Link to="/login">already if have account Login here</Link>
+            </div>
+        </GreetingImageContainer>}/>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css'
+import './media.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
